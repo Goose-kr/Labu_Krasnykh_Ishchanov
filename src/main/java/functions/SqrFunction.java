565cf7efbd3864d6865ffab7a81e775daa81cd32
Home <Goose-kr>
2020-09-16 21:05:20 +0400

    Добавлены тест класса SqrFunction и добавлен его тестовый класс

---
+++ src/main/java/functions/SqrFunction.java
@@ -0,0 +1,8 @@
+package functions;
+import java.lang.Math;
+public class SqrFunction {
+    double apply (double x) {
+        double y=2;
+        return Math.pow(x,y);
+    }
+}
---
+++ src/test/java/functions/SqrFunctionTest.java
@@ -0,0 +1,10 @@
+package functions;
+
+import static org.testng.Assert.*;
+
+public class SqrFunctionTest {
+    double apply (double x) {
+        double y=2;
+        return Math.pow(x,y);
+    }
+}
