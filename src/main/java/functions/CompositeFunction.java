6d7b6c3d56584a8a1a28045d5bcf8281c5f5980a
Home <Goose-kr>
2020-09-19 08:42:18 +0400

    Созданны конструктор Functions и метод apply для данного класса

--- src/main/java/functions/CompositeFunction.java
+++ src/main/java/functions/CompositeFunction.java
@@ -1,12 +1,17 @@
 package functions;
 
+import java.lang.Math;
+
 public class CompositeFunction {
     private MathFunction firstFunction;
     private MathFunction secondFunction;
 
-    void setFunctions(MathFunction firstFunction, MathFunction secondFunction) {
+    public void Functions(MathFunction firstFunction, MathFunction secondFunction) {
         this.firstFunction = firstFunction;
         this.secondFunction = secondFunction;
     }
 
+    double apply(double x) {
+        return secondFunction.apply(firstFunction.apply(x));
+    }
 }
---
+++ src/test/java/functions/CompositeFunctionTest.java
@@ -0,0 +1,7 @@
+package functions;
+
+import static org.testng.Assert.*;
+
+public class CompositeFunctionTest {
+
+}
