08795148806ce86992e1fd69688315b96615f73e
Gumirishche <gumer2001@mail.ru>
2020-10-09 22:20:48 +0400

    Создан класс-тест ArrayTabulatedFunctionTest

---
+++ src/test/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/functions/ArrayTabulatedFunctionTest.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions;
+
+import static org.testng.Assert.*;
+
+public class ArrayTabulatedFunctionTest {
+    double[]x={1,2,3};
+    double[]y={4,8,12};
+    ArrayTabulatedFunction arrayTabulatedFunction=new ArrayTabulatedFunction();
+}
