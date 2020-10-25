a8c19cc735994038215fa98806fb03ecfa550e38
Gumirishche <gumer2001@mail.ru>
2020-10-25 10:30:36 +0400

    Создан класс Point

---
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/functions/Point.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
