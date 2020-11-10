94c4e77147d2a45c743868e9b798a766f80583d8
Gumirishche <gumer2001@mail.ru>
2020-11-10 11:11:00 +0400

    Сделаны тесты asPoints, создан класс InconsistentFunctionsException

---
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,11 @@
+package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+    public InconsistentFunctionsException() {
+    }
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}
+
--- src/test/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/operations/TabulatedFunctionOperationServiceTest.java
+++ src/test/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/operations/TabulatedFunctionOperationServiceTest.java
@@ -1,7 +1,32 @@
 package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.operations;
 
+import org.testng.annotations.Test;
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.Point;
+
 import static org.testng.Assert.*;
 
 public class TabulatedFunctionOperationServiceTest {
+    final double[] xValues1 = new double[]{-2, -1, 0, 1, 2};
+    final double[] yValues1 = new double[]{4, 1, 0, 1, 4};
+    ArrayTabulatedFunction arrayTabulatedFunction = new ArrayTabulatedFunction(xValues1, yValues1);
+    LinkedListTabulatedFunction linkedListTabulatedFunction = new LinkedListTabulatedFunction(xValues1, yValues1);
 
+    @Test
+    public void testAsPoints() {
+        Point[] points = TabulatedFunctionOperationService.asPoints(arrayTabulatedFunction);
+        int i = 0;
+        for (Point point : points) {
+            assertEquals(point.x, arrayTabulatedFunction.getX(i), 0.000001);
+            assertEquals(point.y, arrayTabulatedFunction.getY(i++), 0.000001);
+        }
+        assertEquals(i, arrayTabulatedFunction.getCount());
+        i = 0;
+        for (Point point : points) {
+            assertEquals(point.x, linkedListTabulatedFunction.getX(i), 0.000001);
+            assertEquals(point.y, linkedListTabulatedFunction.getY(i++), 0.000001);
+        }
+        assertEquals(i, linkedListTabulatedFunction.getCount());
+    }
 }
