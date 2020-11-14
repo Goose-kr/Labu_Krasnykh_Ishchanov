6654f8ff1652af278b0f8ac58b0e2790118290bf
Home <Goose-kr>
2020-11-14 23:00:42 +0400

    Дописан класс TabulatedDifferentialOperator

--- src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/operations/TabulatedDifferentialOperator.java
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/operations/TabulatedDifferentialOperator.java
@@ -1,5 +1,6 @@
 package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.operations;
 
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.Point;
 import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.TabulatedFunction;
 import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.factory.ArrayTabulatedFunctionFactory;
 import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.factory.TabulatedFunctionFactory;
@@ -8,12 +9,20 @@ public class TabulatedDifferentialOperator implements DifferentialOperator<Tabul
     private TabulatedFunctionFactory factory;
         @Override
         public TabulatedFunction derive(TabulatedFunction function) {
-            return function;
+            int count = function.getCount();
+            Point[] points = TabulatedFunctionOperationService.asPoints(function);
+            double[]xValues = new double[count-1];
+            double[]yValues = new double[count-1];
+            for(int i=0; i<count-1;i++){
+                yValues[i] = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
+                xValues[i] = points[i].x;
+            }
+            return factory.create(xValues,yValues);
         }
 
         @Override
         public double apply(double x) {
-            return 0;
+            return x;
         }
         public void setFactory(TabulatedFunctionFactory factory){
             this.factory=factory;
