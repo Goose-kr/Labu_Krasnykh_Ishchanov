6d20cdecdbb18fba81ac482128b83d6ad0296adf
Home <Goose-kr>
2020-11-19 20:09:51 +0400

    Работает класс TabulatedDifferentialOperator и его тесты

--- src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/operations/TabulatedDifferentialOperator.java
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/operations/TabulatedDifferentialOperator.java
@@ -11,12 +11,15 @@ public class TabulatedDifferentialOperator implements DifferentialOperator<Tabul
         public TabulatedFunction derive(TabulatedFunction function) {
             int count = function.getCount();
             Point[] points = TabulatedFunctionOperationService.asPoints(function);
-            double[]xValues = new double[count-1];
-            double[]yValues = new double[count-1];
+            double[]xValues = new double[count];
+            double[]yValues = new double[count];
             for(int i=0; i<count-1;i++){
                 yValues[i] = (points[i + 1].y - points[i].y) / (points[i + 1].x - points[i].x);
                 xValues[i] = points[i].x;
             }
+            yValues[count-1]=(points[count-2].y -points[count-1].y)/(points[count-2].x-points[count-1].x);
+            xValues[count-1]=points[count-1].x;
+
             return factory.create(xValues,yValues);
         }
 
