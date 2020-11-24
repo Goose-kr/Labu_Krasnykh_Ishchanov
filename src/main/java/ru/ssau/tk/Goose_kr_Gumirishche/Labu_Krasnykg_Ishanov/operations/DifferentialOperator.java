c60b042fd4941ebf106647aacae018de8dbdaff3
Gumirishche <gumer2001@mail.ru>
2020-11-24 10:40:48 +0400

    исправления

--- src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/operations/DifferentialOperator.java
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/operations/DifferentialOperator.java
@@ -2,7 +2,7 @@ package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.operations;
 
 import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.MathFunction;
 
-public interface DifferentialOperator<T> extends MathFunction {
+public interface DifferentialOperator<T extends MathFunction>{
     T derive(T function);
 
 }
