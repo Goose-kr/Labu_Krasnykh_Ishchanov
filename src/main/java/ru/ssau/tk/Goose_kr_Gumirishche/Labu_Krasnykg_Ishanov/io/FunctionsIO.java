58dee79ec3b9a4d285dedf1a3e238a3983cb6535
Gumirishche <gumer2001@mail.ru>
2020-11-24 10:52:01 +0400

    начало 3 задания

---
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/io/FunctionsIO.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.io;
+
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+
+public final class FunctionsIO {
+    public FunctionsIO(){
+        throw new UnsupportedOperationException();
+    }
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+
+    }
+}
