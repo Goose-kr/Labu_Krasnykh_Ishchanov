9ad779c34aff2cd9151c73967a008d1253cb5b5c
Home <Goose-kr>
2020-12-03 23:03:03 +0400

    Создан класс ReadWriteTask, а также создан класс ReadWriteTaskExecutor

---
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/concurrent/ReadWriteTaskExecutor.java
@@ -0,0 +1,28 @@
+package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.concurrent;
+
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.TabulatedFunction;
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.ZeroFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadWriteTaskExecutor {
+    public static void main(String[] args) {
+        TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(new ZeroFunction(), 1, 10, 10);
+        List<Thread> threadList = new ArrayList<>();
+        for (int i = 1; i<=20;i++){
+            Thread newTread = new Thread(new ReadWriteTask(tabulatedFunction));
+            threadList.add(newTread);
+        }
+        for (Thread currentTread: threadList){
+            currentTread.start();
+        }
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
