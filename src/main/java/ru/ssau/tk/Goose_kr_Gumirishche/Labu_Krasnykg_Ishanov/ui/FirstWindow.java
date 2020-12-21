d42d8bcafdf1b00f75241e02fb87092983ca74e0
Home <Goose-kr>
2020-12-21 18:47:38 +0400

    Создание таблицы

--- src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/ui/FirstWindow.java
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/ui/FirstWindow.java
@@ -16,7 +16,7 @@ public class FirstWindow extends JFrame {
         Container container = this.getContentPane();
         JLabel label = new JLabel("Input size:");
         container.add(label);
-        JTextField input = new JTextField("This is text field");
+        JTextField input = new JTextField(" ");
         container.add(input);
         JButton button = new JButton("enter");
         container.add(button);
@@ -27,19 +27,61 @@ public class FirstWindow extends JFrame {
             }
         });
     }
+
         /*GroupLayout layout=new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setAutoCreateGaps(true);
         layout.setAutoCreateContainerGaps(true);
         JScrollPane tableScrollPane =new JScrollPane(table);
-    }
-
-    public class MyTableModel extends AbstractTableModel{
-
     }*/
 
+    public class MyTableModel extends AbstractTableModel {
+        private static final int INDEX_COLUMN_NUMBER = 0;
+        private static final int VALUE_X_COLUMN_NUMBER = 1;
+        private static final int VALUE_Y_COLUMN_NUMBER = 2;
+        private List strings;
+
+        public MyTableModel(List strings) {
+            this.strings = strings;
+        }
+
+        @Override
+        public int getRowCount() {
+
+            return strings.size;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 2;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            switch (columnIndex) {
+               case INDEX_COLUMN_NUMBER:
+                    return rowIndex;
+                case VALUE_X_COLUMN_NUMBER:
+                    return strings.get(rowIndex);
+                case VALUE_Y_COLUMN_NUMBER:
+                    return strings.get(rowIndex);
+            }
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+
+        public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+            if (columnIndex != VALUE_COLUMN_NUMBER) {
+                return;
+            }
+            strings.set(rowIndex, String.valueOf(aValue));
+        }
+
+    }
+
     public static void main(String[] args) {
-        JFrame firstWindow=new FirstWindow();
+        JFrame firstWindow = new FirstWindow();
         firstWindow.setVisible(true);
     }
 }
