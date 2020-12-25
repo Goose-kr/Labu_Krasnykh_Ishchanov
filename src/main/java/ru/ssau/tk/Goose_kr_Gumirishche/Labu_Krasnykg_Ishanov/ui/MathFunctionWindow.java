aa9f664c97170c10ad5f26cc7bb1967c49b9807d
Gumirishche <gumer2001@mail.ru>
2020-12-25 22:22:12 +0400

    доделал меню и выбор фабрики начал делать математические функции, добавил ползунок)

---
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/ui/MathFunctionWindow.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.ui;
+
+import javax.swing.*;
+
+public class MathFunctionWindow extends JDialog {
+    public MathFunctionWindow(){
+        super();
+    }
+}
--- src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/ui/Table.java
+++ src/main/java/ru/ssau/tk/Goose_kr_Gumirishche/Labu_Krasnykg_Ishanov/ui/Table.java
@@ -1,7 +1,10 @@
 package ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.ui;
 
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.TabulatedFunction;
 import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.factory.LinkedListTabulatedFunctionFactory;
 import ru.ssau.tk.Goose_kr_Gumirishche.Labu_Krasnykg_Ishanov.functions.factory.TabulatedFunctionFactory;
 
 import javax.swing.*;
@@ -9,6 +12,7 @@ import javax.swing.table.AbstractTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 public class Table extends JDialog {
@@ -18,8 +22,11 @@ public class Table extends JDialog {
     private final AbstractTableModel tableModelY;
     private final JTable tableX;
     private final JTable tableY;
+    private TabulatedFunction function;
+    private static final int ARRAY_FUNCTION=0;
+    private static final int LINKED_LIST_FUNCTION=1;
 
-    public Table(int size) {
+    public Table(int size, int number) {
         super();
         stringsY = new ArrayList<>(size);
         stringsX = new ArrayList<>(size);
@@ -29,6 +36,7 @@ public class Table extends JDialog {
         }
         tableModelX = new TableModelX(stringsX);
         tableModelY = new TableModelY(stringsY);
+        WindowEvent event = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
         tableX = new JTable(tableModelY);
         tableY = new JTable(tableModelX);
         setSize(new Dimension(500, 500));
@@ -38,7 +46,8 @@ public class Table extends JDialog {
         create.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+                TabulatedFunctionFactory factoryArray = new ArrayTabulatedFunctionFactory();
+                TabulatedFunctionFactory factoryList = new LinkedListTabulatedFunctionFactory();
                 double[] xValues = new double[size];
                 double[] yValues = new double[size];
                 for (int i = 0; i < size; i++) {
@@ -47,7 +56,13 @@ public class Table extends JDialog {
                     System.out.println(xValues[i]);
                     System.out.println(yValues[i]);
                 }
-                factory.create(xValues, yValues);
+                switch (number) {
+                    case ARRAY_FUNCTION:
+                        function = factoryArray.create(xValues, yValues);
+                    case LINKED_LIST_FUNCTION:
+                        function = factoryList.create(xValues, yValues);
+                }
+                dispose();
             }
         });
         GroupLayout layout = new GroupLayout(getContentPane());
@@ -56,9 +71,13 @@ public class Table extends JDialog {
         layout.setAutoCreateContainerGaps(true);
         JScrollPane tableScrollPaneX = new JScrollPane(tableX);
         JScrollPane tableScrollPaneY = new JScrollPane(tableY);
-        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER).addGroup(layout.createSequentialGroup().addComponent(tableScrollPaneX).addComponent(tableScrollPaneY)).addGroup(layout.createSequentialGroup().addComponent(create)));
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER).addGroup(layout.createSequentialGroup().addComponent(tableScrollPaneY).addComponent(tableScrollPaneX)).addGroup(layout.createSequentialGroup().addComponent(create)));
 
         layout.setVerticalGroup(layout.createSequentialGroup().addGroup(layout.createParallelGroup().addComponent(tableScrollPaneX).addComponent(tableScrollPaneY)).addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING).addComponent(create)));
         setLocationRelativeTo(null);
+    }
+
+    public TabulatedFunction getFunction() {
+        return function;
     }
 }
